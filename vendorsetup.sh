dd4babb482a450cd03f040199a87e2f39cc2ee0e
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2023-06-29 20:07:20 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,5 +4,4 @@ git clone https://github.com/ronitkrshah/android_device_oneplus_CPH2381-kernel.g
 # Clone hardware/oplus from LineageOS
 git clone https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 
-# kernel source 
-git clone https://github.com/nobiajay/android_kernel_sm6375.git kernel/oneplus/sm6375
+
