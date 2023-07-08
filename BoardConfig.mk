726d85250b8aeef409a88d32d6dfedc0db1091b2
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2023-07-08 22:15:44 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
 
-TODO
+# TODO
 # TARGET_KERNEL_SOURCE := kernel/oneplus/sm6375
 
 # CMD LINE KERNEL 
