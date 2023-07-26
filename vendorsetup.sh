075000e265ee2611a62daf7c7ce2429ab24c1a28
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2023-07-26 08:00:46 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# Clone Prebuilt Kernel
+git clone https://github.com/ronitkrshah/android_device_oneplus_CPH2381-kernel.git device/oneplus/CPH2381-kernel
+
+# Clone hardware/oplus from LineageOS
+git clone https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
+
