d9c2c70eab6dc8363f7bc5fdcce8cbb4d7f77eb9
AJY <135806539+nobiajay@users.noreply.github.com>
2023-07-30 15:58:17 +0530

    Change name to rasgulla

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_CPH2381.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_CPH2381-user \
-    lineage_CPH2381-userdebug \
-    lineage_CPH2381-eng
+    lineage_rasgulla-user \
+    lineage_rasgulla-userdebug \
+    lineage_rasgulla-eng
